014a46535e06614d8c756376149e472673fc6144
Oliver Tomlinson <development+dotdestroyer@esendex.com>
2014-10-26 08:21:58 +0000

    adasdf

--- SmsByPost.WebApp/API/DispatchEventController.cs
+++ SmsByPost.WebApp/API/DispatchEventController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Web.Http;
+using PusherServer;
 using SmsByPost.Models;
 using SmsByPost.Services;
 
@@ -25,6 +26,9 @@ namespace SmsByPost.API
             var letter = _azureBlobService.GetFromBlobStore(id);
             letter.Events.Add(new Event() { EventName = "sms_dispatched_event", ScheduledDateTimeUtc = scheduledDateTimeUtc });
             _azureBlobService.UploadToAzureBlobStore(letter);
+
+            var pusher = new Pusher("94194", "09e07fa6d1e3db728a17", "a1f339dc466359b5915b");
+            var result = pusher.Trigger(new[] { letter.Address, id.ToString() }, "sms_dispatched_event", new { message = letter.Message, msisdn = letter.Address });
 
             _messageEventService.EnqueueMessage(letter);
             
